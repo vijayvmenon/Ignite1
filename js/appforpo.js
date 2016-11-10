eb31a65b79c9814ef31f642fa844ce47468809a0
Vijay Menon <vijay.menon@cognizant.com>
2016-11-10 02:19:20 -0600

    added backend service to add pivotal api

--- js/appforpo.js
+++ js/appforpo.js
@@ -1,17 +1,15 @@
-var appforpo = angular.module('appforpo',['LocalStorageModule','ui.grid','ui.grid.expandable','ui.grid.saveState', 'ui.grid.selection', 'ui.grid.pinning', 'ui.bootstrap','ui-notification']);
+var appforpo = angular.module('appforpo',['ui.grid','ui.grid.expandable','ui.grid.saveState', 'ui.grid.selection', 'ui.grid.pinning', 'ui.bootstrap','ui-notification']);
 
 
 
-appforpo.controller('MainCtrl', ['$scope', '$http','localStorageService','$interval', '$modal', '$log', '$window', function ($scope, $http, localStorageService,$interval, $modal, $log,$window) {
+appforpo.controller('MainCtrl', ['$scope', '$http','gridFactory','$interval', '$modal', '$log', '$window', function ($scope, $http,gridFactory,$interval, $modal, $log,$window) {
 
  $scope.gotomain = function(){
 
     $window.location.href="index.html"
     };
 
- if(localStorageService.isSupported) {
-  console.log('local storage is supported');
-}
+
 
   $scope.gridOptions = {
     expandableRowTemplate: 'purchase_order_expanded.html',
@@ -29,15 +27,26 @@ appforpo.controller('MainCtrl', ['$scope', '$http','localStorageService','$inter
      this.overrideEnable=false
      }
   },
-  persistData:function (slot) {
-   localStorage.setItem("gridData",JSON.stringify($scope.gridOptions.data));
+  persistData:function (currentRow) {
+    console.log(currentRow);
+    gridFactory.updDet(currentRow)
+  .success(function(data, status, headers, config) {
+    console.log('success!!' + '\n\n' + data+'\n'+status+'\n'+headers+'\n'+config);
+  })
+  .error(function(data, status, headers, config) {
+    console.log('error!!' + '\n\n' + data+'\n'+status+'\n'+headers+'\n'+config);
+  });
   },
   noMsgFirst:0,
   persistDataOverride:function (currentRow) {
-   localStorage.setItem("gridData",JSON.stringify($scope.gridOptions.data)),
-  // localStorage.setItem("suprvsrData",JSON.stringify(currentRow)),
-   this.overrideEnable=false,
-   console.log(currentRow)
+   gridFactory.updDet(currentRow)
+  .success(function(data, status, headers, config) {
+    console.log('success!!' + '\n\n' + data+'\n'+status+'\n'+headers+'\n'+config);
+   this.overrideEnable=false
+     })
+    .error(function(data, status, headers, config) {
+    console.log('error!!' + '\n\n' + data+'\n'+status+'\n'+headers+'\n'+config);
+      });
   },
   dueQty:function(totqty,rcvqty) {
     return totqty - rcvqty;
@@ -59,16 +68,16 @@ appforpo.controller('MainCtrl', ['$scope', '$http','localStorageService','$inter
     }
   }
 
-
    $scope.gridOptions.columnDefs = [
-     { name: 'PoNbr' },
-     { name: 'ItemNbr'},
-     { name: 'TotQty',enableFiltering: false },
-     { name: 'DueQty',enableFiltering: false },
-     { name: 'DeliveryNo' },
-     { name: 'UserID'},
-     { name: 'Status'},
-     { name: 'Timestamp',enableFiltering: false},
+     { name: 'poNbr'},
+     { name: 'itemNbr'},
+     { name: 'totalQty',enableFiltering: false},
+     { name: 'rcvdQty',enableFiltering: false},
+     { name: 'deliveryNbr'},
+     { name: 'userId'},
+     { name: 'poStatus'},
+    // { name: 'rcvdTs',type: 'date', cellFilter: 'date:\'yyyy-MM-dd hh:mm:ss\'',enableFiltering: false,width: 250},
+     { name: 'doorNbr'},
      {
 	name: 'Receive',
 	displayName:' ',
@@ -83,254 +92,21 @@ appforpo.controller('MainCtrl', ['$scope', '$http','localStorageService','$inter
 	 }
   ];
 
+gridFactory.getDet()
+.success(function(response) {
+  console.log(response);
+ $scope.gridOptions.data=response;  
+ if($scope.gridOptions.data[0].isLocked == "true") {
+  console.log('entered');
+ }
+})
+.error(function() {
+  console.log("error!!");
+});
 
 
-var gridData = [{
-    "PoNbr": 11111,
-    "ItemNbr": 10000,
-    "TotQty": 21,
-    "LabelNo": "3456737989212-A001",
-    "UserID": "vmenon",
-    "DoorNo": 101,
-    "RcvQty":0,
-    "Timestamp": "02/01/2016 21:00:00",
-    "DeliveryNo": "12345",
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":false
-  }, {
-    "PoNbr": 11111,
-    "ItemNbr": 10001,
-    "TotQty": 23,
-    "LabelNo": "3456743567214-B001",
-    "UserID": "vmenon",
-    "DoorNo": 102,
-    "RcvQty":23,
-    "Timestamp": "02/01/2016 21:00:00",
-    "DeliveryNo": "12345",
-    "Status": "Received",
-    "SlotId":"A001",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":true     
-  }, {
-    "PoNbr": 22222,
-    "ItemNbr": 10003,
-    "TotQty": 35,
-    "LabelNo": "3456787623428-D001",
-    "UserID": "vmenon",
-    "DoorNo": 103,
-    "RcvQty":0,
-    "Timestamp": "02/01/2016 21:00:00",
-    "DeliveryNo": "12346",
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":false     
-  }, {
-    "PoNbr": 22222,
-    "ItemNbr": 10004,
-    "TotQty": 38,
-    "LabelNo": "4356287324519-D001",
-    "UserID": "manney",
-    "DoorNo": 101,
-    "RcvQty":0,
-    "Timestamp": "02/02/2016 21:00:00",
-    "DeliveryNo": 12346,
-    "Status": "Created",
-   "SlotId":"",
-    "NoPallet":0,
-    "Comments":"", 
-    "isLocked":false
-  }, {
-    "PoNbr": 33333,
-    "ItemNbr": 10005,
-    "TotQty": 40,
-    "LabelNo": "4356267834510-C001",
-    "UserID": "manney",
-    "DoorNo": 105,
-    "RcvQty":0,
-    "Timestamp": "02/02/2016 21:00:00",
-    "DeliveryNo": 12346,
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-      "isLocked":false    
-  }, {
-    "PoNbr": 33333,
-    "ItemNbr": 10006,
-    "TotQty": 41,
-    "LabelNo": "4356298732434-A001",
-    "UserID": "manney",
-    "DoorNo": 103,
-    "RcvQty":41,
-    "Timestamp": "02/02/2016 21:00:00",
-    "DeliveryNo": 12347,
-    "Status": "Received",
-    "SlotId":"D001",
-    "NoPallet":0,
-    "Comments":"",
-   "isLocked":true     
-  }, {
-    "PoNbr": 44444,
-    "ItemNbr": 10004,
-    "TotQty": 21,
-    "LabelNo": "6541245892100-B001",
-    "UserID": "ugonugu",
-    "DoorNo": 102,
-    "RcvQty":0,
-    "Timestamp": "02/03/2016 21:00:00",
-    "DeliveryNo": 12347,
-    "Status": "Created",
-   "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":false    
-  }, {
-    "PoNbr": 44444,
-    "ItemNbr": 10007,
-    "TotQty": 31,
-    "LabelNo": "6541256721416-A001",
-    "UserID": "ugonugu",
-    "DoorNo": 104,
-    "RcvQty":31,
-    "Timestamp": "02/03/2016 21:00:00",
-    "DeliveryNo": 12348,
-    "Status": "Received",
-    "SlotId":"B001",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":true     
-  }, {
-    "PoNbr": 55555,
-    "ItemNbr": 10008,
-    "TotQty": 21,
-    "LabelNo": "6541256892621-D001",
-    "UserID": "ugonugu",
-    "DoorNo": 101,
-    "RcvQty":0,
-    "Timestamp": "02/03/2016 21:00:00",
-    "DeliveryNo": 12348,
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":false    
-  }, {
-    "PoNbr": 55555,
-    "ItemNbr": 10006,
-    "TotQty": 27,
-    "LabelNo": "5439812387611-B001",
-    "UserID": "b0naga",
-    "DoorNo": 105,
-    "RcvQty":0,
-    "Timestamp": "02/04/2016 21:00:00",
-    "DeliveryNo": 12348,
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-      "isLocked":false    
-  }, {
-    "PoNbr": 66666,
-    "ItemNbr": 10010,
-    "TotQty": 25,
-    "LabelNo": "5439829845615-A001",
-    "UserID": "b0naga",
-    "DoorNo": 102,
-    "RcvQty":25,
-    "Timestamp": "02/04/2016 21:00:00",
-    "DeliveryNo": 12349,
-    "Status": "Received",
-    "SlotId":"A001",
-    "NoPallet":0,
-    "Comments":"",
-      "isLocked":true     
-  }, {
-    "PoNbr": 66666,
-    "ItemNbr": 10011,
-    "TotQty": 34,
-    "LabelNo": "5439890876500-A001",
-    "UserID": "b0naga",
-    "DoorNo": 104,
-    "RcvQty":0,
-    "Timestamp": "02/04/2016 21:00:00",
-    "DeliveryNo": 12349,
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-      "isLocked":false    
-  }, {
-    "PoNbr": 66666,
-    "ItemNbr": 10012,
-    "TotQty": 9,
-    "LabelNo": "7643529078602-C001",
-    "UserID": "lachu",
-    "DoorNo": 101,
-    "RcvQty":9,
-    "Timestamp": "02/05/2016 21:00:00",
-    "DeliveryNo": 12349,
-    "Status": "Received",
-    "SlotId":"C001",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":true       
-  }, {
-    "PoNbr": 77777,
-    "ItemNbr": 10013,
-    "TotQty": 17,
-    "LabelNo": "7643581954610-D001",
-    "UserID": "lachu",
-    "DoorNo": 104,
-    "RcvQty":0,
-    "Timestamp": "02/05/2016 21:00:00",
-    "DeliveryNo": 12350,
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-      "isLocked":false    
-  }, {
-    "PoNbr": 88888,
-    "ItemNbr": 10014,
-    "TotQty": 11,
-    "LabelNo": "7643529876311-B001",
-    "UserID": "lachu",
-    "DoorNo": 102,
-    "RcvQty":0,
-    "Timestamp": "02/05/2016 21:00:00",
-    "DeliveryNo": 12350,
-    "Status": "Created",
-    "SlotId":"",
-    "NoPallet":0,
-    "Comments":"",
-    "isLocked":false    
-  }
 
-]
-
- // $http.get('data7.json')
-//    .success(function(data) {
- //    $scope.gridOptions.data = data
-
-//If the gridData is not set in local storage, get the variable above and set it. If its already set, assign it to gridoptions.data
-if (localStorage.getItem('gridData') === null) {
-localStorage.setItem("gridData",JSON.stringify(gridData));
-$scope.gridOptions.data=JSON.parse(localStorage.getItem("gridData"));
-console.log($scope.gridOptions.data);
-}
-else {
-  $scope.gridOptions.data=JSON.parse(localStorage.getItem("gridData"));
-  console.log($scope.gridOptions.data);
-}
    $scope.toggle = true;
-
-
     $scope.$watch('toggle', function(){
         $scope.buttontext = $scope.toggle ? 'Show' : 'Hide';
     });   $scope.toggle = true;
@@ -339,6 +115,7 @@ else {
         $scope.buttontext = $scope.toggle ? 'Show' : 'Hide';
     });
 	
+
 $scope.slotid = [
 { id:1, name: 'A001' },
 { id:2, name: 'B001' },
@@ -349,6 +126,19 @@ $scope.slotid = [
 //$scope.selectedOption = $scope.options[0];
 
 }]);
+
+appforpo.factory('gridFactory',function($http) {
+  return {
+    ser_var:"service variable",
+        getDet:function() {
+      return $http.get('http://receiving.cfapps.io/getallpoinfo');
+    },
+
+    updDet:function(data) {
+      return $http.put('http://receiving.cfapps.io/updatepoinfo',data,{})
+    }
+  }
+})
 
 
 
